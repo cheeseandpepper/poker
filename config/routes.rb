0c68ebd7e35f722f0637a0ae11625d5891ec5e2a
Mike Lerner <cheeseandpepper@gmail.com>
2022-03-27 21:35:36 -0700

    undoing search

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :players,  only: [:index]
   resources :games,    only: [:index]
   resources :hands,    only: [:index]
-  resources :searches, only: [:index]  
+  
   root "games#index"
 end
